298f23e0d5251a601980e63179c6441482d80517
isupov <t.1113@ya.ru>
2022-10-18 18:48:16 +0300

    Added Dots component

--- src/components/Slider/components/controls/Dots.js
+++ src/components/Slider/components/controls/Dots.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SliderContext } from "../../Slider";
+import Dot from "./Dot";
 
-const Dots = () => {
-    return (
-        <>
+export default function Dots() {
+    const { slidesCount } = useContext( SliderContext );
 
-        </>
-    );
+    const renderDots = () => {
+        const dots = [];
+        for ( let i = 0; i < slidesCount; i++ ) {
+            dots.push( <Dot key={ `dot-${ i }` } number={ i }/> )
+        }
+
+        return dots;
+    };
+
+    return <div className="dots">{ renderDots() }</div>;
 };
-
-export default Dots;
