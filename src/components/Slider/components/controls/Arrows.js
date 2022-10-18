6085b170e6e6d84caf4e205377e36839b59ed7ff
isupov <t.1113@ya.ru>
2022-10-18 18:20:27 +0300

    Added Arrows component

--- src/components/Slider/components/controls/Arrows.js
+++ src/components/Slider/components/controls/Arrows.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SliderContext, sliderContext } from "../../Slider";
 
-const Arrows = () => {
+export default function Arrows() {
+    const { changeSlide } = useContext(SliderContext);
+
     return (
-        <>
-
-        </>
+        <div className="arrows">
+            <div className="arrow left" onClick={() => changeSlide(-1)} />
+            <div className="arrow right" onClick={() => changeSlide(1)} />
+        </div>
     );
 };
-
-export default Arrows;
