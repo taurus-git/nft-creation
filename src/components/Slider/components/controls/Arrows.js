90b7fb81052da0382f9b436a38574b722280802e
isupov <t.1113@ya.ru>
2022-10-18 18:40:29 +0300

    Created Dot component

--- src/components/Slider/components/controls/Arrows.js
+++ src/components/Slider/components/controls/Arrows.js
@@ -2,12 +2,12 @@ import React, { useContext } from 'react';
 import { SliderContext, sliderContext } from "../../Slider";
 
 export default function Arrows() {
-    const { changeSlide } = useContext(SliderContext);
+    const { changeSlide } = useContext( SliderContext );
 
     return (
         <div className="arrows">
-            <div className="arrow left" onClick={() => changeSlide(-1)} />
-            <div className="arrow right" onClick={() => changeSlide(1)} />
+            <div className="arrow left" onClick={ () => changeSlide( -1 ) }/>
+            <div className="arrow right" onClick={ () => changeSlide( 1 ) }/>
         </div>
     );
 };
--- src/components/Slider/components/controls/Dot.js
+++ src/components/Slider/components/controls/Dot.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SliderContext } from "../../Slider";
 
-const Dot = () => {
+export default function Dot( { number } ) {
+    const { goToSlide, sliderNumber } = useContext( SliderContext )
+
     return (
-        <>
-
-        </>
+        <div
+            className={ `dot ${ sliderNumber === number ? "selected" : "" }` }
+            onClick={ () => goToSlide( number ) }
+        />
     );
 };
-
-export default Dot;
