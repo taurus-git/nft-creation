071a29c9058ad5593bc48a7c516be38a27834e06
isupov <t.1113@ya.ru>
2022-10-21 11:27:48 +0300

    Added alt tag

--- src/components/Slider/components/Slide.js
+++ src/components/Slider/components/Slide.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import SlideImage from "./SlideImage";
 
-export default function Slide( { data: { banner_image_url } } ) {
+export default function Slide( { data: { banner_image_url, description } } ) {
+
     return (
         <div className="slide">
-            <SlideImage src={ banner_image_url }/>
+            <SlideImage src={ banner_image_url } alt={ description }/>
         </div>
     );
 };
--- src/components/Slider/components/SlideImage.js
+++ src/components/Slider/components/SlideImage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export default function SlideImage({src}) {
-    const getImageSrcSet = (set) => {
-        return src.replace( "w=500", `w=${set}` );
+export default function SlideImage( { src, alt } ) {
+    const getImageSrcSet = ( set ) => {
+        return src.replace( "w=500", `w=${ set }` );
     }
 
     return (
         <picture>
-            <source srcSet={`${getImageSrcSet(640)} 1x, ${getImageSrcSet(750)} 2x`}
-                    media="(max-width: 600px)" />
-            <img srcSet={`${getImageSrcSet(1080)} 1x, ${getImageSrcSet(2048)} 2x`} />
+            <source srcSet={ `${ getImageSrcSet( 640 ) } 1x, ${ getImageSrcSet( 750 ) } 2x` }
+                    media="(max-width: 600px)"/>
+            <img srcSet={ `${ getImageSrcSet( 1080 ) } 1x, ${ getImageSrcSet( 2048 ) } 2x` } alt={ alt }/>
         </picture>
     );
 };
