f35476f8bff9d97bf3055f7e4022b9f2cb7cb551
isupov <t.1113@ya.ru>
2022-10-21 17:53:23 +0300

    Added basic markup for buttons

--- src/components/Slider/components/Slide.js
+++ src/components/Slider/components/Slide.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import SlideImage from "./SlideImage";
+import Button from "../../Button/Button";
 
 export default function Slide( { data: { banner_image_url, name, description } } ) {
 
     return (
         <div className="slide">
             <div className="slide__info">
-                <h1 className="slide__title">{name}</h1>
-                <p className="slide__description">{description}</p>
+                <h1 className="slide__title">{ name }</h1>
+                <p className="slide__description">{ description }</p>
+            </div>
+            <div className="slide__buttons">
+                <Button class="slide__buttons-cta">
+                    Create
+                </Button>
+                <Button class="slide__buttons-cta">
+                    Explore
+                </Button>
             </div>
             <SlideImage src={ banner_image_url } alt={ description }/>
         </div>
