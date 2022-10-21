1765dc952c9727fd94fb29c3239fc004dc69387b
isupov <t.1113@ya.ru>
2022-10-21 18:30:33 +0300

    Added basic button styles

--- src/components/Slider/components/Slide.js
+++ src/components/Slider/components/Slide.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SlideImage from "./SlideImage";
 import Button from "../../Button/Button";
+import Menu from "../../Menu/Menu";
+import ModalWrapper from "../../../layouts/container/Modal/ModalWrapper";
 
 export default function Slide( { data: { banner_image_url, name, description } } ) {
+    const [isMenuOpen, setMenuOpen] = useState( false );
 
+    const menuList = [
+        {
+            "title": "Explore NFT's",
+            "link": "https://www.google.com/"
+        },
+        {
+            "title": "Explore Categories",
+            "link": "https://mail.google.com/mail/u/0/?ogbl#inbox"
+        },
+        {
+            "title": "Explore Collections",
+            "link": "https://www.youtube.com/"
+        },
+    ];
+
+    const handleNavButtonClick = () => {
+       console.log(153);
+        // setMenuOpen( !isMenuOpen );
+    }
     return (
         <div className="slide">
             <div className="slide__info">
@@ -11,10 +33,18 @@ export default function Slide( { data: { banner_image_url, name, description } }
                 <p className="slide__description">{ description }</p>
             </div>
             <div className="slide__buttons">
-                <Button class="slide__buttons-cta">
+                <Button onClick={ handleNavButtonClick } class="button--nav slider__cta" icon="#chevron">
                     Create
                 </Button>
-                <Button class="slide__buttons-cta">
+
+                <ModalWrapper class={ isMenuOpen ? "btn-menu" : "" }
+                              opacityClass={ isMenuOpen ? "open" : "" }
+                              onClick={ handleNavButtonClick }
+                              content={ <Menu menuList={ menuList }
+                                              class={ isMenuOpen ? "open" : "" }/>
+                              }/>
+
+                <Button class="button--nav slider__cta--black" icon="#chevron">
                     Explore
                 </Button>
             </div>
