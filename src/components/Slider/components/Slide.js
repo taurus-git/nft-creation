386333a31548dbe92adce81abeb4bbaa0f83f0a5
isupov <t.1113@ya.ru>
2022-10-21 13:14:58 +0300

    Added basic styles for view title and description

--- src/components/Slider/components/Slide.js
+++ src/components/Slider/components/Slide.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import SlideImage from "./SlideImage";
 
-export default function Slide( { data: { banner_image_url, description } } ) {
+export default function Slide( { data: { banner_image_url, name, description } } ) {
 
     return (
         <div className="slide">
+            <div className="slide__info">
+                <h1 className="slide__title">{name}</h1>
+                <p className="slide__description">{description}</p>
+            </div>
             <SlideImage src={ banner_image_url } alt={ description }/>
         </div>
     );
