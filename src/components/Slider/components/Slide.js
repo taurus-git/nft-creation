d8b2674fc01c02f4d27be2ddf501625e2a57f031
isupov <t.1113@ya.ru>
2022-11-02 14:08:32 +0200

    Added a Todo commit

--- src/components/Slider/components/Slide.js
+++ src/components/Slider/components/Slide.js
@@ -1,8 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SlideImage from "./SlideImage";
 import Button from "../../Button/Button";
-import Menu from "../../Menu/Menu";
-import ModalWrapper from "../../../layouts/container/Modal/ModalWrapper";
 
 export default function Slide( { data: { banner_image_url, name, description } } ) {
     const [isMenuOpen, setMenuOpen] = useState( false );
@@ -23,8 +21,8 @@ export default function Slide( { data: { banner_image_url, name, description } }
     ];
 
     const handleNavButtonClick = () => {
-       console.log('clicked');
-        // setMenuOpen( !isMenuOpen );
+        console.log( 'clicked' ); //Todo: add a menu on click
+        setMenuOpen( !isMenuOpen );
     }
 
     return (
