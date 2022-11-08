50d2527e353c0590a72b646d6caac19250aae306
isupov <t.1113@ya.ru>
2022-11-08 18:44:33 +0200

    Added unique key for promo boxes

--- src/components/PromoBoxes/Box.js
+++ src/components/PromoBoxes/Box.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Box = ( { content: {title, description } } ) => {
+const Box = ( { content: { title, description } } ) => {
     return (
         <div className="promo-boxes__box">
-            <h4 className="promo-boxes__title">{title}</h4>
-            <p className="promo-boxes__description">{description}</p>
+            <h4 className="promo-boxes__title">{ title }</h4>
+            <p className="promo-boxes__description">{ description }</p>
         </div>
     );
 };
--- src/components/PromoBoxes/PromoBoxes.js
+++ src/components/PromoBoxes/PromoBoxes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Box from "./Box";
+import { generateKey } from "../../services/Helpers/helpers";
 
 const PromoBoxes = props => {
     return (
         <section className="promo-boxes">
             { props.boxes.map( ( box ) => (
-                <Box content={ box }/>
+                <Box key={ generateKey( box.title ) } content={ box }/>
             ) ) }
         </section>
     );
